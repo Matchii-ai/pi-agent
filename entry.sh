57eac9624fcefa9ab28145d17792da28a982f8ee
naisanzaa <naisanza@gmail.com>
2026-07-04 23:55:08 -0700

    entry

--- entry.sh
+++ entry.sh
@@ -4,16 +4,11 @@ set -e
 
 PRIME_PROMPT="$(cat ~/brain/*.md 2>/dev/null || echo)"
 
-if [[ ! -z "$@" ]]; then
-  set -x
-  exec pi --approve $@
-fi
-
 if [[ ! -z $PRIME_PROMPT ]]; then
   set -x
   exec pi --approve $@ "$(cat ~/brain/*.md)"
 fi
 
 set -x
-exec pi --approve
+exec pi --approve $@
 
