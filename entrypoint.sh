c38d1eb8edcdf6d191a9bc6ab87a95c26e4953da
naisanzaa <naisanza@gmail.com>
2026-07-11 08:17:05 -0700

    there are some lingering sessions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ RESTORE="$(echo $@ | grep -oP '(?<=--name )[a-zA-Z0-9_-]*')"
 set -e
 
 if [[ ! -z $RESTORE ]]; then
-    if grep -r ".name.:.$RESTORE" $HOME/.pi >/dev/null 2>&1 ; then
+    if grep -r ".name.:.$RESTORE" $HOME/.pi/agent/sessions >/dev/null 2>&1 ; then
 	set -x
 	exec pi --approve -r "$@"
     fi 
